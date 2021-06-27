52bf9588b25eeb30c75297ce0dd7eb46e877d5a5
unknown <farhan.fadila1717@gmail.com>
2021-06-27 15:58:31 +0700

    Update example

--- example/example.dart
+++ example/example.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:loading_card/loading_card.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: ExampleLoadingCard(),
-      ),
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example Loading Card',
+      debugShowCheckedModeBanner: false,
+      home: ExampleLoadingCard(),
     );
+  }
+}
 
 class ExampleLoadingCard extends StatelessWidget {
   @override
